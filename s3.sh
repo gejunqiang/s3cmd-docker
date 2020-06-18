fae36f9603c16c47ff0350c7c871451434c04348
gejunqiang <gejunqiang@corp.netease.com>
2020-06-18 11:28:32 +0800

    update s3.sh

--- s3.sh
+++ s3.sh
@@ -2,14 +2,14 @@
 
 export AWS_ACCESS_KEY_ID="$ACCESS_KEY"
 export AWS_SECRET_ACCESS_KEY="$SECRET_KEY"
-export AWS_DEFAULT_REGION="$REGION"
+export HOST_BUCKET="$HOST_BUCKET"
 
 s3cmd(){
   local SSL_PARAM="--no-ssl"
   [[ "${SSL,,}" =~ ^(true|t|yes|y|on|1|enable|enabled)$ ]] && {
       SSL_PARAM=""
   }
-  /root/s3cmd-2.1.0/s3cmd --host="$ENDPOINT" --host-bucket="$ENDPOINT" "$SSL_PARAM" "$@"
+  /root/s3cmd-2.1.0/s3cmd --host="$ENDPOINT" --host-bucket="${HOST_BUCKET:-$ENDPOINT}" --region="$REGION" "$SSL_PARAM" "$@"
 }
 
 s3cmd "$@"
